5d90c1ddbf550c153d5960fff76e4644843001e2
John Ferland <johnferlandmi@gmail.com>
2022-01-14 14:10:45 -0500

    commit 6

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo "Installing 'node-cmd'"
 npm install node-cmd
 echo "Installing 'node-ssh'"
 npm install node-ssh
-echo "Running 'server.js'"
-node server.js
+echo "Running 'deploy.js'"
+node deploy.js
